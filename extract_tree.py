a5f6cf3215fcca243168ee3557bb295c78618e80
cathuan <geli7630@gmail.com>
2018-01-20 16:39:35 +1100

    Construct a dictionary containing all trees

--- extract_tree.py
+++ extract_tree.py
@@ -2,43 +2,47 @@ from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 
+"""
+tree_ is object in sklearn.tree.tree_.Tree
+tree is a dictionary node_id -> TreeNode(node_id)
+"""
 
 class TreeNode(object):
 
     # node_id is a non-negative integer
     # node_id = 0 represents the root of the tree
-    # tree is the sklearn.tree.tree_.Tree object, which is a cython
+    # tree_ is the sklearn.tree.tree_.Tree object, which is a cython
     # class object.
-    def __init__(self, node_id, tree, feature_names=None):
+    def __init__(self, node_id, tree_, feature_names=None):
         assert node_id >= 0
         self.node_id = node_id
-        self.threshold = tree.threshold[node_id]
+        self.threshold = tree_.threshold[node_id]
         if feature_names is not None:
-            self.feature = feature_names[tree.feature[node_id]]
+            self.feature = feature_names[tree_.feature[node_id]]
         else:
-            self.feature = "X[%s]" % tree.feature[node_id]
+            self.feature = "X[%s]" % tree_.feature[node_id]
 
 
         # handle old & new version of trees. In 0.13.1 and 0.19.1,
-        # the implementation of tree has been changed.
-        if hasattr(tree, "init_error"):
-            self.impurity = tree.init_error[node_id]
-        elif hasattr(tree, "impurity"):
-            self.impurity = tree.impurity[node_id]
+        # the implementation of tree_ has been changed.
+        if hasattr(tree_, "init_error"):
+            self.impurity = tree_.init_error[node_id]
+        elif hasattr(tree_, "impurity"):
+            self.impurity = tree_.impurity[node_id]
         else:
             assert False, "what is your sklearn version? No init_error nor impurity"
 
-        if hasattr(tree, "n_samples"):
-            self.n_samples = tree.n_samples[node_id]
-        elif hasattr(tree, "n_node_samples"):
-            self.n_samples = tree.n_node_samples[node_id]
+        if hasattr(tree_, "n_samples"):
+            self.n_samples = tree_.n_samples[node_id]
+        elif hasattr(tree_, "n_node_samples"):
+            self.n_samples = tree_.n_node_samples[node_id]
         else:
             assert False, "what is your sklearn version? No n_samples nor n_node_samples"
 
         # calculate n_samples of each category and percentage of each
         # category
-        self.value = tree.value[node_id]
-        if tree.n_outputs == 1:
+        self.value = tree_.value[node_id]
+        if tree_.n_outputs == 1:
             self.value = self.value[0,:]
         self.percents = [round(v*1.0/self.n_samples, 4) for v in self.value]
 
@@ -47,12 +51,20 @@ class TreeNode(object):
 
     def __repr__(self):
 
-        return "%s <= %.4f\n" % (self.feature, self.threshold) + \
+        return "=== TreeNode %s ===\n" % self.node_id + \
+               "%s <= %.4f\n" % (self.feature, self.threshold) + \
                "gini = %.4f\n" % self.impurity + \
                "samples = %s\n" % self.n_samples + \
                "value = %s\n" % self.value + \
                "percents = %s" % self.percents
 
+
+def get_tree(tree_, feature_names=None):
+
+    tree = {}
+    for node_id in range(len(tree_.value)):
+        tree[node_id] = TreeNode(node_id, tree_, feature_names)
+    return tree
 
 
 if __name__ == "__main__":
@@ -62,6 +74,7 @@ if __name__ == "__main__":
     clf = DecisionTreeClassifier(max_depth=2)
     clf.fit(iris.data, iris.target)
 
-    tree = clf.tree_
-    root = TreeNode(0, tree)
-    print root
+    tree_ = clf.tree_
+    tree = get_tree(tree_)
+    for node in tree.values():
+        print node
